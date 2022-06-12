f30b834c6734f227c5281d11c6b57a3cee0366ac
restuhanputra <restu.hanputraa@gmail.com>
2022-06-12 23:29:04 +0700

    update: challenge07 with concat & filter

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let dataIXX = await promiseTheaterIXX();
+  let dataVGC = await promiseTheaterVGC();
+  let dataTheater = dataIXX.concat(dataVGC); //merge dataIXX & dataVGC -> buat array baru
+
+  let result = dataTheater.filter((item) => item.hasil === emosi);
+  return result.length;
+};
 
 module.exports = {
   promiseOutput,
